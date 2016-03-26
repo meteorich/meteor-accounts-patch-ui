6632494a5dbc76064b33aeca1121f7ff6269d41a
Lee Pender <lpender@gmail.com>
2016-03-26 19:33:45 -0400

    Update `blaze` version constraint.
    Attempt to solve: https://github.com/brettle/meteor-accounts-patch-ui/issues/6

--- package.js
+++ package.js
@@ -48,7 +48,7 @@ Package.onUse(function(api) {
 
   // We monkey patch the _helpers and _eventMaps Template properties blaze
   // manages.
-  api.use('blaze@=2.1.3 || =2.1.2 || =2.1.1 || =2.1.0', 'client');
+  api.use('blaze@2.1.0', 'client');
   // Among other things, we assume that the hooks in the options object are used
   // directly, not copied.
   api.use('iron:router@=1.0.12 || =1.0.11 || =1.0.10 || =1.0.9', 'client', 
